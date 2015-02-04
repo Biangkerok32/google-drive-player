00e0025c6d533a5366f7cf83fc1c50065de46a4e
Brandon Silva <itbrandonsilva@gmail.com>
2015-02-04 07:57:37 -0800

    Configuration through environment.

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ var express = require("express");
 var http = require("http");
 var path = require("path");
 var fs = require("fs");
+var swig = require("swig");
 
 var app = express();
+var PORT = parseInt(process.env.GDP_PORT) || 8086;
+var DOMAIN = process.env.GDP_DOMAIN || "ex.com";
 
-app.set("port", process.env.PORT || 80);
+app.set("port", PORT);
 app.use(app.router);
 console.log(__dirname);
 app.use(express.directory(__dirname));
 app.use(express.static(__dirname));
 
-app.get("/", function (req, res) { res.send(fs.readFileSync(path.join(__dirname, "index.html"), {encoding: "utf8"})); });
+app.get("/", function (req, res) { 
+    res.send(fs.readFileSync(path.join(__dirname, "index.html"), {encoding: "utf8"}));
+});
+
+var appjs = swig.compileFile(path.join(__dirname, "/src/app.js"));
+app.get("/src/app.js", function (res, res) {
+    res.send(appjs({domain: DOMAIN}));
+});
+
 app.get("/oauth2callback", function (req, res) { res.send("<html></html>"); });
 
 http.createServer(app).listen(app.get("port"), function () {
